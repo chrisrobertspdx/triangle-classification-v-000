5f0920c602c798d84e519856bb5fe54f5a50f49a
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-02 17:51:42 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,8 +10,8 @@ class Triangle
   def kind
     if self.a + self.b < self.c || self.a + self.c < self.b || self.b + self.c < self.a || self.b + self.c + self.a == 0
       begin
-        raise ShapeError
-      rescue ShapeError => error
+        raise TriangleError
+      rescue TriangleError => error
           puts error.message
       end
     elsif [self.a,self.b,self.c].uniq.size == 1
@@ -22,7 +22,7 @@ class Triangle
       :scalene
     end
   end
-  class ShapeError < StandardError
+  class TriangleError < StandardError
     def message
       "Shape does nnot meet spatial requirements to be a triangle"
     end
