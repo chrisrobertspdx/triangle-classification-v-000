9fbb37329495f4fb8a44d7cfd702d8bab8d08d53
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-02 17:53:16 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,15 +6,16 @@ class Triangle
     @a = a
     @b = b
     @c = c
-  end
-  def kind
     if self.a + self.b < self.c || self.a + self.c < self.b || self.b + self.c < self.a || self.b + self.c + self.a == 0
       begin
         raise TriangleError
       rescue TriangleError => error
           puts error.message
       end
-    elsif [self.a,self.b,self.c].uniq.size == 1
+    end
+  end
+  def kind
+    if [self.a,self.b,self.c].uniq.size == 1
       :equilateral
     elsif [self.a,self.b,self.c].uniq.size == 2
       :isosceles
